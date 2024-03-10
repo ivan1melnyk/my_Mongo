6180200bc49815a33b35dde0c55424c7144cccf1
ivan1melnyk <melnyk.ivan20a@gmail.com>
2024-03-10 13:12:00 +0200

    very important changes

--- load_files.py
+++ load_files.py
@@ -2,7 +2,7 @@ from models import Author, Quote
 import json
 from mongoengine import connect
 
-connection_string = "mongodb+srv://melnykivan20a:<password>@ivan20a.c0mc4dz.mongodb.net/?retryWrites=true&w=majority&appName=ivan20a"
+connection_string = "mongodb+srv://melnykivan20a:h9WecjRmh0w6VvD8@ivan20a.c0mc4dz.mongodb.net/?retryWrites=true&w=majority&appName=ivan20a"
 connect('mydatabase', host=connection_string)
 
 
@@ -12,7 +12,8 @@ def load_authors_from_json(file_path):
         authors_data = json.load(file)
         # result_authors = db.mydatabase.insert_many(authors_data)
         for author in authors_data:
-            author_record = Author(fullname=author["fullname"], born_date=author["born_date"], born_location=author["born_location"], description=author["description"])
+            author_record = Author(fullname=author["fullname"], born_date=author["born_date"],
+                                   born_location=author["born_location"], description=author["description"])
             author_record.save()
 
 
